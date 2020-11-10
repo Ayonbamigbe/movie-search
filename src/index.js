eda9ef0c60fb6951a12aadf8ef92735245f277cc
ayonbamigbe <ayox2012@gmail.com>
2020-11-10 19:15:47 +0100

    comit 2020 11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import "./index.css";
 import SearchMovies from "./searchMovies";
 
 class Main extends React.Component {
--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
+import MovieCard from "./MovieCard";
+
 export default function SearchMovies() {
 
     const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
-    
+
     const searchMovies = async (e) => {
         e.preventDefault();
         
 
-        const url = `https://api.themoviedb.org/3/search/movie/?api_key=5e87ae2d66a6bcca76ece81c68d48b23&language=en=US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie/?api_key=5e87ae2d66a6bcca76ece81c68d48b23&language=en=US&query=${query}&page=1&include_adult=true`;
 
         try {
             const res = await fetch(url);
@@ -37,24 +39,13 @@ export default function SearchMovies() {
                     type = 'submit'>
                     Search 
                 </button> 
-
-                <div className="card-list">
-                    {movies.filter(movie => movie.poster_path).map(movie => (
-                        <div className="card" key={movie.id}>
-                            <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                            alt={movie.title + 'poster'} 
-                            />
-                            <div className="card--content">
-                            <h3 className="card--title">{movie.title}</h3>
-                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                            <p><small>RATING: {movie.vote_average}</small></p>
-                            <p className="card--desc">{movie.overview}</p>
-                            </div>
-                        </div>
-                    )
-                    )}
-                </div>
             </form>
+
+            <div className = "card-list"> 
+                {movies.filter(movie => movie.poster_path).map(movie =>(
+                    <MovieCard movie={movie} key={movie.id} />
+                ))} 
+            </div>
         </>
     )
 
